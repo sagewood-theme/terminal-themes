387cedd74971431280b5d9ca1de803ba96434259
sagewood-theme <angad.sigma420@gmail.com>
2026-07-20 17:54:12 +1000

    Create .wezterm.lua

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,22 @@
+-- Official Sage Theme - WezTerm Port
+local wezterm = require 'wezterm'
+
+return {
+  colors = {
+    background = "#2d3833",
+    foreground = "#d2ded7",
+    cursor_bg = "#d2ded7",
+    cursor_fg = "#2d3833",
+    selection_bg = "#3b4a43",
+    selection_fg = "#d2ded7",
+
+    ansi = {
+      "#222a26", "#d99c82", "#a8b59b", "#d6caa3",
+      "#8da7b3", "#caacc2", "#9bb5b1", "#d2ded7"
+    },
+    brights = {
+      "#677a70", "#d99c82", "#a8b59b", "#d6caa3",
+      "#8da7b3", "#caacc2", "#9bb5b1", "#d2ded7"
+    },
+  }
+}
